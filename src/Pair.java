c3137b4b5214d111ecac99fb6e6dcb983a7c069c
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2023-05-03 20:55:10 -0400

    Finalized Driver

--- src/Pair.java
+++ src/Pair.java
@@ -1,23 +1,47 @@
+/**
+ * Class to hold an index and a value.
+ */
 public class Pair implements Comparable
 {
+    /** integer value */
     private int index;
+    /** double value paired with the index. */
     private double value;
 
+    /**
+     * Specific constructor
+     * @param index the integer index.
+     * @param value the double value.
+     */
     public Pair(int index, double value)
     {
         this.index = index;
         this.value = value;
     }
 
+    /**
+     * Get the index.
+     * @return int index.
+     */
     public int getIndex()
     {
         return index;
     }
+
+    /**
+     * Get the value
+     * @return double value.
+     */
     public double getValue()
     {
         return value;
     }
 
+    /**
+     * Compare two Pair Objects. used with Comparable Interface.
+     * @param o the object to be compared.
+     * @return negative number or positive number based on which value is greater.
+     */
     @Override
     public int compareTo(Object o) {
         Pair other = (Pair)o;
